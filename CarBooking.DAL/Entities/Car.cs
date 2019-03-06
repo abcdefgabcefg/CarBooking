a363c43ab07593ba3eee59dcd49400ba6266f93e
Riadinsky Alexandr <riadinsky.alex@gmail.com>
2019-03-06 23:28:03 +0200

    Add validation to create car index

--- CarBooking.DAL/Entities/Car.cs
+++ CarBooking.DAL/Entities/Car.cs
@@ -7,27 +7,31 @@ using System.ComponentModel.DataAnnotations;
 
 namespace CarBooking.DAL.Entities
 {
-    // don't forget to change repository updating logic
     public class Car
     {
         public int ID { get; set; }
 
         [Required(ErrorMessage = requireMes)]
+        [Display(Name ="Luxury:")]
         public bool IsLuxury { get; set; }
 
         // in dollars per hour
         [Required(ErrorMessage = requireMes)]
+        [Display(Name ="Price: ")]
         public decimal Price { get; set; }
 
         [Required(ErrorMessage = requireMes)]
         [MaxLength(60, ErrorMessage = "Max lenght is 60 characters")]
+        [Display(Name ="Car: ")]
         public string CarTitle { get; set; }
 
         [Required(ErrorMessage =requireMes)]
+        [Display(Name ="Free: ")]
         public bool IsFree { get; set; }
 
         [Required(ErrorMessage =requireMes)]
         [MaxLength(300)]
+        [Display(Name ="Image Path: ")]
         public string ImagePath { get; set; }
 
         private const string requireMes = "Required field";
---
+++ CarBooking.WEB/MyScripts/carDataValidation.js
@@ -0,0 +1,50 @@
+﻿document.forms[0].addEventListener('submit', function (event) {
+    let result = true;
+
+    const carTitle = document.querySelector('#CarTitle').value;
+    const carTitleError = document.querySelector('#car-title-error');
+    carTitleError.textContent = '';
+
+    if (carTitle === '') {
+        carTitleError.textContent = 'Required field';
+        result = false;
+    }
+    else if (carTitle.length > 60) {
+        carTitleError.textContent = 'Max lenght is 60 characters';
+        result = false;
+    }
+
+    const price = document.querySelector('#Price').value;
+    const priceError = document.querySelector('#price-error');
+    priceError.textContent = '';
+
+    if (price === '') {
+        priceError.textContent = 'Required field';
+        result = false;
+    }
+    else if (isNaN(price)) {
+        priceError.textContent = 'Need to be number';
+        result = false;
+    }
+    else if (price <= 0) {
+        priceError.textContent = 'Need to be grater than zero';
+        result = false;
+    }
+
+    const imagePath = document.querySelector('#ImagePath').value;
+    const imagePathError = document.querySelector('#image-path-error');
+    imagePathError.textContent = '';
+
+    if (imagePath === '') {
+        imagePathError.textContent = 'Required field';
+        result = false;
+    }
+    else if (imagePath.length > 300) {
+        imagePathError.textContent = 'Max lenght is 300 characters';
+        result = false;
+    }
+
+    if (!result) {
+        event.preventDefault();
+    }
+})
